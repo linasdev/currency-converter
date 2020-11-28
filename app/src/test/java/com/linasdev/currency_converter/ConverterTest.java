145ae522306db1944440419bc43a5c9a96e354e5
Linas Nikiperavičius <linas@linasdev.com>
2020-11-28 18:17:56 +0200

    Implement conversion tests

--- app/src/test/java/com/linasdev/currency_converter/ConverterTest.java
+++ app/src/test/java/com/linasdev/currency_converter/ConverterTest.java
@@ -23,16 +23,35 @@ public class ConverterTest {
     
     @Test
     public void conversionToBaseCurrencyShouldBeCorrect() {
-        // TODO: implement test;
+        Converter converter = new Converter("EUR", 18, RoundingMode.DOWN);
+        converter.registerCurrency("USD", BigDecimal.valueOf(0.809552722));
+
+        BigDecimal expected = new BigDecimal("0.099944780312927107");
+        BigDecimal result = converter.convert("EUR", "USD", BigDecimal.valueOf(0.1234567899));
+
+        assertEquals(expected, result);
     }
     
     @Test
     public void conversionFromBaseCurrencyShouldBeCorrect() {
-        // TODO: implement test;
+        Converter converter = new Converter("EUR", 18, RoundingMode.DOWN);
+        converter.registerCurrency("USD", BigDecimal.valueOf(0.809552722));
+        
+        BigDecimal expected = new BigDecimal("0.152499999746773749");
+        BigDecimal result = converter.convert("USD", "EUR", BigDecimal.valueOf(0.1234567899));
+
+        assertEquals(expected, result);
     }
     
     @Test
     public void conversionOfArbitraryCurrenciesShouldBeCorrect() {
-        // TODO: implement test;
+        Converter converter = new Converter("EUR", 18, RoundingMode.DOWN);
+        converter.registerCurrency("USD", BigDecimal.valueOf(0.809552722));
+        converter.registerCurrency("BTC", BigDecimal.valueOf(6977.089657));
+
+        BigDecimal expected = new BigDecimal("1064.006170925717749979");
+        BigDecimal result = converter.convert("USD", "BTC", BigDecimal.valueOf(0.1234567899));
+
+        assertEquals(expected, result);
     }
 }
