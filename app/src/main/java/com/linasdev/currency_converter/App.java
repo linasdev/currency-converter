f0013060a5aed8df16d06b633eb68d28deb20909
Linas Nikiperavičius <linas@linasdev.com>
2020-11-28 19:58:39 +0200

    Define CLI

--- app/src/main/java/com/linasdev/currency_converter/App.java
+++ app/src/main/java/com/linasdev/currency_converter/App.java
@@ -1,6 +1,47 @@
 package com.linasdev.currency_converter;
 
-public class App {
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.List;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ArgGroup;
+
+@Command(
+    name = "Currency Converter",
+    version = "Currency Converter 1.0",
+    mixinStandardHelpOptions = true)
+public class App implements Runnable {
+    private static class ConversionRequest {
+        @Parameters(index = "0", description = "Identifier of target currency.")
+        String sourceCurrency;
+
+        @Parameters(index = "1", description = "Identifier of source currency.")
+        String targetCurrency;
+
+        @Parameters(index = "2", description = "Amount of source currency.")
+        BigDecimal amount;
+    }
+
+    @Option(
+        names = { "-r", "--rates" },
+        description = "CSV file containing the exchange rates.",
+        defaultValue = "./exchange_rates.csv",
+        paramLabel = "file") 
+    private File exchangeRateDatabase;
+
+    @ArgGroup(exclusive = false, multiplicity = "1..*")
+    private List<ConversionRequest> conversionRequests;
+
+    @Override
+    public void run() {
+    }
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args); 
+        System.exit(exitCode);
     }
 }
